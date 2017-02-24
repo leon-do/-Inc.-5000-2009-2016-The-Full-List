7ace106ceb1ee6046e63a0141301c668dcfbb858
Leon Do <leondo123@gmail.com>
2017-02-24 14:56:13 -0500

    moved stuff around. building front end

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var express = require('express')
 var app = express()
-var data = require('./data.js')
+var data = require('./data/data.js')
  
 
+//display the api on: http://52.14.154.146/
 app.get('/', function (req, res) {
     res.json(data.inc)
 })
- 
 app.listen(80)
